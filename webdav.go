26e857ac29d981aa5e3e7c093c4391a8de5b0e76
Henrique Dias <hacdias@gmail.com>
2019-07-05 07:55:45 +0100

    chore: update caddy (#21)
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- webdav.go
+++ webdav.go
@@ -9,8 +9,8 @@ import (
 	wd "golang.org/x/net/webdav"
 
 	"github.com/hacdias/webdav/webdav"
-	"github.com/mholt/caddy"
-	"github.com/mholt/caddy/caddyhttp/httpserver"
+	"github.com/caddyserver/caddy"
+	"github.com/caddyserver/caddy/caddyhttp/httpserver"
 )
 
 func init() {
